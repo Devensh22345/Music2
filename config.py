3d229c0a921b9c0284f4724d562f45da4569ff20
Devensh22345 <kumardevensh0@gmail.com>
2024-07-29 19:51:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Learningbots79/Learning_Bots",
+    "https://github.com/Devensh22345/Music2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
