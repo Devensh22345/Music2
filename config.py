3c95d5c479df60dc29603ef38dc26f0b8560778e
Devensh22345 <kumardevensh0@gmail.com>
2024-07-29 19:45:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,8 +29,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "@DK_x_BOT")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "DKxBOT_Group")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/DK_X_BOT")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/DKXBOT_group")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
